b890223f6a3d2b4d462711d9a0465c3b1bb38b60
synergy2411 <synergy2411@outlook.com>
2019-08-07 10:24:50 +0530

    data service added

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,12 +3,12 @@ import { USER_DATA } from '../model/mocks';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class DataService{
-  constructor(private httpClient : HttpClient){}
-  getData(){
-  return USER_DATA;
+export class DataService {
+  constructor(private httpClient: HttpClient) { }
+  getData() {
+    return USER_DATA;
   }
-  getJSONData(){
+  getJSONData() {
     return this.httpClient.get("assets/model/user-data.json");
   }
 }
